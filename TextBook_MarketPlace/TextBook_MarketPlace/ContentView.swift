769d9cc35f8dbd97b7ffbeb5de661d901293f84f
George Jacob <georgekj1998@gmail.com>
2022-04-27 12:00:45 -0500

    Initial Commit

---
+++ TextBook_MarketPlace/TextBook_MarketPlace/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TextBook_MarketPlace
+//
+//  Created by George Jacob on 4/27/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TextBook_MarketPlace/TextBook_MarketPlace/TextBook_MarketPlaceApp.swift
@@ -0,0 +1,17 @@
+//
+//  TextBook_MarketPlaceApp.swift
+//  TextBook_MarketPlace
+//
+//  Created by George Jacob on 4/27/22.
+//
+
+import SwiftUI
+
+@main
+struct TextBook_MarketPlaceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
